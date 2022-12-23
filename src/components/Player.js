9bd614222b8b0280b0d37c2bc9cd65416ac53948
Numissa11 <61435050+Numissa11@users.noreply.github.com>
2022-12-23 23:32:03 +0100

    player control buttons added

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PlayerControls from './PlayerControls'
 import PlayerDetails from './PlayerDetails'
 
 function Player({nextSong, song}) {
@@ -7,6 +8,7 @@ function Player({nextSong, song}) {
         <audio></audio>
         <h4>Playing now</h4>
         <PlayerDetails song={song} />
+        <PlayerControls />
         <p><strong>Next up : </strong>{nextSong.name} by {nextSong.artist}</p>
     </div>
   )
--- src/components/PlayerDetails.js
+++ src/components/PlayerDetails.js
@@ -4,7 +4,7 @@ function PlayerDetails({song}) {
   return (
     <div className='c-player-details'>
       <div className='details-img'>
-        <img src={song.cover} />
+        <img alt='cover' src={song.cover} />
       </div>
       <h3 className='details-title' >{song.title}</h3>
       <h4 className='details-artist'>{song.artist}</h4>
