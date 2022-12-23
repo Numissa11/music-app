a03b4ea529b25080afa6a53bc0ed0300475d1642
Numissa11 <61435050+Numissa11@users.noreply.github.com>
2022-12-23 22:03:24 +0100

    setup components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import React, { useState } from 'react';
+import chillHop from './data'
 
 function App() {
+  const [songs, setSongs] = useState([
+    
+  ])
+
   return (
     <div className="App">
-      App
+      {songs}
     </div>
   );
 }
