4a5df4356115c6dd40aac39d9fef3e8f14387d69
Numissa11 <61435050+Numissa11@users.noreply.github.com>
2022-12-27 12:29:58 +0100

    just for fun: hire me

--- src/resources/data.js
+++ src/resources/data.js
@@ -48,9 +48,9 @@ const chillHop = () => {
       active: false,
     },
     {
-      name: "Under the City Stars",
+      name: "Hire...me",
       cover: "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
-      artist: "Aso, Middle School, Aviino",
+      artist: "Aline Sinclaire",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
       color: ["#205950", "#2ab3bf"],
       id: uuidv4(),
